bde47198c0ec536ffb7e3cfd464bcfb0763b5970
Joseph Terranova <terranovajoseph1@gmail.com>
2023-10-19 12:24:51 -0400

    added a users oject

--- client/src/Model/Users.ts
+++ client/src/Model/Users.ts
@@ -9,11 +9,3 @@ export interface User {
   role: "admin" | "user",
   token?: string
 }
-
-export function getUsers(): User[] {
-  return data.users.map( x => ({ ...x, role: x.id <= 5 ? 'admin' : 'user' }) ) 
-}
-
-export function getUserByEmail(email: string): User | undefined {
-  return getUsers().find( x => x.email === email );
-}
