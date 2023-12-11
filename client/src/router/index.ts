53851a5462aeacfbfb1c52c639954bbf9a3a2358
Joseph Terranova <terranovajoseph1@gmail.com>
2023-12-11 15:09:14 -0500

    trimmed some more stuff

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -28,14 +28,6 @@ const router = createRouter({
       component: () => import('../views/FriendsActivity.vue')
     },
 
-    {
-      path: '/friendslist',
-      name: 'friendslist',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/FriendsListView.vue')
-    },
 
     {
       path: '/peoplesearch',
